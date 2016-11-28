cc0fcd9678951ed6c864ee42e0f78fa52a3ce584
SteinarTh <SteinarTh>
2016-11-28 16:05:18 +0000

    Read input from file

--- datalayer.cpp
+++ datalayer.cpp
@@ -1,6 +1,64 @@
 #include "datalayer.h"
+#include "persons.h"
+
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 DataLayer::DataLayer()
 {
 
+    vector<Persons> myVec;
+    string input, name;
+    int bY, dY;
+    char gender;
+    bool alive;
+    ifstream inStream;
+    inStream.open("textFile.txt");
+    int counter = 0;
+
+    while(inStream >> input)
+    {
+        //TODO:
+        if(counter % 5 == 0 )    // Name
+        {
+            name = input;
+
+        }
+        if(counter % 5 == 1)    // birthYear
+        {
+            int value = atoi(input.c_str());
+          bY = value;
+
+        }
+        if(counter % 5 == 2)    //deathYear
+        {
+            int value = atoi(input.c_str());
+            dY= value;
+        }
+        if(counter % 5 == 3)    //gender
+        {
+            int value = atoi(input.c_str());
+            gender = value;
+
+        }
+        if(counter % 5 == 4)    //Alive
+        {
+            if (input == "true")
+            {
+               alive = true;
+            }
+            else
+            {
+                alive = false;
+            }
+
+            Persons newPerson(name, bY, dY, gender, alive);
+            myVec.push_back(newPerson);
+        }
+        counter++;
+    }
 }
