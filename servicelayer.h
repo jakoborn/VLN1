b82ff801be0f3f48d4919837487554bbd268b6fb
Jakob Örn <jakobog16@ru.is>
2016-11-28 14:59:04 +0000

    vector bætt við

--- servicelayer.h
+++ servicelayer.h
@@ -1,9 +1,17 @@
 #ifndef SERVICELAYER_H
 #define SERVICELAYER_H
 
+#include <vector>
+#include "datalayer.h"
+
+using namespace std;
 
 class ServiceLayer
 {
+private:
+
+    vector <DataLayer> persons;
+
 public:
     ServiceLayer();
 
