2ace5381190e06c6f438c80958e1ee95e681d0e6
SteinarTh <SteinarTh>
2016-12-01 12:57:16 +0000

    corrected search by gender

--- consoleui.cpp
+++ consoleui.cpp
@@ -240,6 +240,10 @@ void ConsoleUI::searchData()
                 char gender;
                 cout << "Enter gender (M/F): ";
                 cin >> gender;
+                if(genderCheck(gender) == false)
+                {
+                    searchData();
+                }
                 vector<int> v_g = serve.searchByGender(gender);
                 if (v_g.size() == 0)
                 {
